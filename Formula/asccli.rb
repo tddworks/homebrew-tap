34adeb5bebaec92f1110e1aa87f574034b8617da
itshan <itshan@tddworks.com>
2026-02-28 09:19:07 +0000

    Update asccli to v0.1.27

--- Formula/asccli.rb
+++ Formula/asccli.rb
@@ -4,23 +4,23 @@
 class Asccli < Formula
   desc "App Store Connect CLI — manage apps, versions, and screenshots from your terminal"
   homepage "https://github.com/tddworks/asc-cli"
-  version "v0.1.25"
+  version "v0.1.27"
   license "MIT"
 
   on_arm do
-    url "https://github.com/tddworks/asc-cli/releases/download/v0.1.25/asc_v0.1.25_macOS_arm64"
-    sha256 "6083e7d3676a0a7e6dc9924d67da5ec555561f1715f588e06877c8d15cd643af"
+    url "https://github.com/tddworks/asc-cli/releases/download/v0.1.27/asc_v0.1.27_macOS_arm64"
+    sha256 "83ff39d195b63d2dab773e632b926cceec7c1f2b70b35d381ca08182f8d46b54"
   end
 
   on_intel do
-    url "https://github.com/tddworks/asc-cli/releases/download/v0.1.25/asc_v0.1.25_macOS_x86_64"
-    sha256 "63cc826398b1e91280b0fae58d6f8b6f9680a7babb51c0b6dfc525a278ec1292"
+    url "https://github.com/tddworks/asc-cli/releases/download/v0.1.27/asc_v0.1.27_macOS_x86_64"
+    sha256 "60f34a920247e06a6bddc85bb06b147696ea99193aa04c22844f15908def6331"
   end
 
   depends_on :macos
 
   def install
-    binary = Hardware::CPU.arm? ? "asc_v0.1.25_macOS_arm64" : "asc_v0.1.25_macOS_x86_64"
+    binary = Hardware::CPU.arm? ? "asc_v0.1.27_macOS_arm64" : "asc_v0.1.27_macOS_x86_64"
     bin.install binary => "asc"
   end
 
