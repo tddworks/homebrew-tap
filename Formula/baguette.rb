b878feab7879b4674243149fb525a88d5649e1e4
itshan <itshan@tddworks.com>
2026-05-03 03:17:51 +0000

    Update baguette to v0.1.4

--- Formula/baguette.rb
+++ Formula/baguette.rb
@@ -4,12 +4,12 @@
 class Baguette < Formula
   desc "Headless iOS Simulator manager + host-side input injection for iOS 26"
   homepage "https://github.com/tddworks/baguette"
-  version "v0.1.3"
+  version "v0.1.4"
   license "Apache-2.0"
 
   on_arm do
-    url "https://github.com/tddworks/baguette/releases/download/v0.1.3/baguette_v0.1.3_macOS_arm64.tar.gz"
-    sha256 "07b3f45d06741ec76116a1aae2a81450466131ad1820a6958181c27c7ed1f0f3"
+    url "https://github.com/tddworks/baguette/releases/download/v0.1.4/baguette_v0.1.4_macOS_arm64.tar.gz"
+    sha256 "04b96ef8189ad0daf8dd24a912e94184ddbfe8691b6b004d5a18854fee6391f2"
   end
 
   depends_on :macos
